3f944f8a9beb2398fdf3860713798b8fcdd31e5b
Ruthishkumar G <ruthishkumar.2353045@srec.ac.in>
2025-06-15 19:18:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,5 +162,6 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
